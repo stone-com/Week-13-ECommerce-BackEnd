18265b63e00005c00a8cf56f3f8fe8a4ff3b5f5d
Stone Comstock <comstockstone@yahoo.com>
2022-03-20 15:30:41 -0700

    findall tags route done

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{ model: Product, through: ProductTag, as: 'products' }],
+  })
+    .then((results) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
