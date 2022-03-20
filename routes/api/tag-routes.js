417cffa24909b8ae1d7a0297438e3613a845b067
Stone Comstock <comstockstone@yahoo.com>
2022-03-20 15:44:17 -0700

    all tag routes done

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -58,10 +58,50 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((results) => {
+      // check if no results(searching for 0 index to see if any data exists at all) respond with 404 and inform user no results found for that ID
+      if (!results[0]) {
+        res.status(404).json({
+          message: `No results found with ID ${req.params.id} found. Please try again with a different ID.`,
+        });
+        return;
+      }
+      // else respond with results
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((results) => {
+      // if no results, respond with 404 and inform user no results found for that ID
+      if (!results) {
+        res.status(404).json({
+          message: `No results found with ID ${req.params.id} found. Please try again with a different ID.`,
+        });
+        return;
+      }
+      // else respond with results
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
